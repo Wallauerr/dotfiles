8bf6b3e5b553c8b4d006a2b42d5beca3e885dcd7
Wallauerr <wallauer@protonmail.com>
2024-05-06 22:51:16 -0300

    feat: add null_ls for format and adjust treesitter to install according to the language

--- init.lua
+++ init.lua
@@ -6,11 +6,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
-    lazypath, 
+    lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("vim-options")
 require("lazy").setup("plugins")
-
