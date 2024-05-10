145ef5f32c35d44f6fca49200397471a88c15d18
Wallauerr <wallauer@protonmail.com>
2024-05-10 12:18:52 -0300

    fix: format codes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,7 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+
   config = function()
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>", {})
   end,
