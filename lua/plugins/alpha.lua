331024e148acb7f2c0bc46ac8af1b0358c284c25
Wallauerr <wallauer@protonmail.com>
2024-05-11 00:08:13 -0300

    feat: add more options in dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,56 +1,60 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "goolord/alpha-nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 
-	config = function()
-		local status_ok, alpha = pcall(require, "alpha")
-		if not status_ok then
-			return
-		end
+  config = function()
+    local status_ok, alpha = pcall(require, "alpha")
+    if not status_ok then
+      return
+    end
 
-		local dashboard = require("alpha.themes.dashboard")
+    local dashboard = require("alpha.themes.dashboard")
 
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
 
-		dashboard.section.buttons.val = {
-			dashboard.button("f", "Find file", ":Telescope find_files <CR>"),
-			dashboard.button("n", "New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("t", "Find text", ":Telescope live_grep <CR>"),
-			dashboard.button("q", "Quit Neovim", ":qa<CR>"),
-		}
+    dashboard.section.buttons.val = {
+      dashboard.button("f", "Find file", ":Telescope find_files <CR>"),
+      dashboard.button("n", "New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("w", "Find word", ":Telescope live_grep <CR>"),
+      dashboard.button("r", "Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("m", "Mason", ":Mason<CR>"),
+      dashboard.button("l", "Lazy", ":Lazy<CR>"),
+      dashboard.button("u", "Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
+      dashboard.button("q", "Quit NVIM", ":qa<CR>"),
+    }
 
-		local function footer()
-			return "Hax0r life"
-		end
+    local function footer()
+      return "Hax0r life"
+    end
 
-		dashboard.section.footer.val = footer()
+    dashboard.section.footer.val = footer()
 
-		dashboard.section.footer.opts.hl = "Type"
-		dashboard.section.header.opts.hl = "Include"
-		dashboard.section.buttons.opts.hl = "Keyword"
+    dashboard.section.footer.opts.hl = "Type"
+    dashboard.section.header.opts.hl = "Include"
+    dashboard.section.buttons.opts.hl = "Keyword"
 
-		dashboard.opts.opts.noautocmd = true
-		alpha.setup(dashboard.opts)
-	end,
+    dashboard.opts.opts.noautocmd = true
+    alpha.setup(dashboard.opts)
+  end,
 }
