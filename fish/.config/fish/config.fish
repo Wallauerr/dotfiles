136fbd86deffb03cf798424ed8b6b7932855a3af
Wallauerr <wallauer@protonmail.com>
2025-04-17 10:35:11 -0300

    feat: add alias to eza tree view

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     starship init fish | source # Commands to run in interactive sessions can go here
     alias l "eza --icons"
+    alias ll "eza --tree --icons"
     alias lg lazygit
 end
