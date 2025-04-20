6740375bc101dfd09342795779e74ba353643dac
wallauerr <wallauer@protonmail.com>
2025-04-19 21:05:21 -0300

    feat: remove welcome message and add title

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,16 @@
 if status is-interactive
-    starship init fish | source # Commands to run in interactive sessions can go here
+    # starship
+    starship init fish | source
+
+    # welcome message
+    #set -g fish_greeting "Welcome to the fish shell"
+
+    # title
+    function fish_title
+        echo -n $USER
+    end
+
+    # alias
     alias l "eza --icons"
     alias ll "eza --tree --icons"
     alias lg lazygit
