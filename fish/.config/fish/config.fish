32dccf7a10df9c275df2f3090c1a71ed3a3f3b9d
Wallauerr <wallauer@protonmail.com>
2025-04-22 08:44:09 -0300

    feat: remove the default fish greeting

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # starship
     starship init fish | source
 
+    # remove the default fish greeting
+    set -U fish_greeting
+
     # welcome message
     #set -g fish_greeting "Welcome to the fish shell"
 
