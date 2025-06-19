61d7e13f0319bcc4e83ad51dc0e3e4436cb878db
wallauerr <wallauer@protonmail.com>
2025-06-19 19:22:15 -0300

    feat: add mcphub.nvim AI plugin with auto-approve config

--- nvim/.config/nvim/lua/plugins/ai.lua
+++ nvim/.config/nvim/lua/plugins/ai.lua
@@ -56,4 +56,21 @@ return {
       },
     },
   },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest",
+    config = function()
+      require("mcphub").setup({
+        auto_approve = true,
+        extensions = {
+          avante = {
+            make_slash_commands = true,
+          },
+        },
+      })
+    end,
+  },
 }
