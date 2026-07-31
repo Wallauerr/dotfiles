58dd933eb8431a3f79912b91e07ee7c561907bda
Vinicius Wallauer <wallauer@protonmail.com>
2026-07-30 21:48:25 -0300

    fix(waybar): use moon icon at night in weather script

--- waybar/.config/waybar/scripts/weather.sh
+++ waybar/.config/waybar/scripts/weather.sh
@@ -10,17 +10,27 @@ fi
 TEMP=$(echo "$DATA" | jq -r '.current_condition[0].temp_C')
 CODE=$(echo "$DATA" | jq -r '.current_condition[0].weatherCode')
 DESC=$(echo "$DATA" | jq -r '.current_condition[0].weatherDesc[0].value')
+ICON_URL=$(echo "$DATA" | jq -r '.current_condition[0].weatherIconUrl[0].value')
+
+NIGHT=false
+[[ "$ICON_URL" == *"night"* ]] && NIGHT=true
 
 case "$CODE" in
-    113) ICON="☀️" ;;
-    116) ICON="⛅" ;;
+    113)
+        if $NIGHT; then ICON="🌙"; else ICON="☀️"; fi
+        ;;
+    116)
+        if $NIGHT; then ICON="🌙"; else ICON="⛅"; fi
+        ;;
     119|122) ICON="☁️" ;;
     143|248|260) ICON="🌫️" ;;
     176|263|266|293|296|299|302|305|308|353|356|359) ICON="🌧️" ;;
     179|182|185|227|230|311|314|317|320|323|326|329|332|335|338|350|362|365|368|371|374|377|395) ICON="❄️" ;;
     200|386|389|392) ICON="⛈️" ;;
     281|284) ICON="🌧️" ;;
-    *) ICON="☀️" ;;
+    *)
+        if $NIGHT; then ICON="🌙"; else ICON="☀️"; fi
+        ;;
 esac
 
 echo "{\"text\": \"$ICON $TEMP°C\", \"tooltip\": \"$DESC\"}"
