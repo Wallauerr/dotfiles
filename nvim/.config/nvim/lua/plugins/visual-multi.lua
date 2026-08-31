a0dd4458a0243372a4f7989edf65106faa22b1bc
Wallauerr <wallauer@protonmail.com>
2026-08-31 11:39:29 -0300

    feat: add vim-visual-multi pluguin

---
+++ nvim/.config/nvim/lua/plugins/visual-multi.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "mg979/vim-visual-multi",
+    keys = {
+      { "<C-n>", desc = "Multicursor: add next occurrence" },
+      { "<C-p>", desc = "Multicursor: add previous occurrence" },
+      { "<C-x>", desc = "Multicursor: skip current occurrence" },
+      { "<C-Up>", desc = "Multicursor: add vertical cursor up" },
+      { "<C-Down>", desc = "Multicursor: add vertical cursor down" },
+    },
+  },
+}
